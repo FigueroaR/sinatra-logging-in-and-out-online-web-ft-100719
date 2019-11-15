dce4125cacb33b3c36d2871e7bc81b4cdca77d8a
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-11-15 05:11:04 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,6 +4,7 @@ class Helpers
         # arg is also a hash {:user_id=>1}
         #binding.pry
     end
+    
     def self.current_user(session)
         # arg is a hash {:user_id=>1}
         @user = User.find_by_id(session[:user_id])
